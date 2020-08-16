15026fad17125e4268fc654ae04df89da5a4bbb3
bbalkon <bbalkonsky@gmail.com>
2020-08-16 19:49:24 +0500

    alpha 0.8.1: refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,50 @@
 <template>
   <div id="app">
-    <p>Выбранный элемент: {{selected}}</p>
+    <p>Выбранный элемент: {{ selected }}</p>
 
-<!--    <c-select v-model="selected" :select-options='a'>Массив строк</c-select>-->
+<!--    <c-select v-model="selected" :select-options='a'>-->
+<!--      <template v-slot:placeholder>Массив строк</template>-->
+<!--    </c-select>-->
     <c-select v-model="selected" :select-options='b' @input="onInput($event)">
       <template v-slot:placeholder>Массив объектов</template>
     </c-select>
-<!--    <c-select v-model="selected" :select-options='c' label-field="name" value-field="id">Массив кастомных объектов</c-select>-->
-<!--    <c-select v-model="selected" :select-options='d'>Стандартный объект</c-select>-->
-<!--    <c-select v-model="selected" :select-options='e'>Кастомный объект</c-select>-->
-<!--    <c-select v-model="selected" :select-options='e' value-field="id">Кастомный объект</c-select>-->
+<!--    <c-select v-model="selected" :select-options='c' label-field="name" value-field="id">-->
+<!--      <template v-slot:placeholder>Массив кастомных объектов</template>-->
+<!--    </c-select>-->
+<!--    <c-select v-model="selected" :select-options='d'>-->
+<!--      <template v-slot:placeholder>Стандартный объект</template>-->
+<!--    </c-select>-->
+<!--    <c-select v-model="selected" :select-options='e'>-->
+<!--      <template v-slot:placeholder>Кастомный объект</template>-->
+<!--    </c-select>-->
+<!--    <c-select v-model="selected" :select-options='e' value-field="id">-->
+<!--      <template v-slot:placeholder>Кастомный объект</template>-->
+<!--    </c-select>-->
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import CSelect from "@/custom-select/CSelect.vue";
+  import {Component, Vue} from 'vue-property-decorator';
+  import CSelect from "@/custom-select/CSelect.vue";
 
-@Component({
-  components: {
-    CSelect
-  },
+  @Component({
+    components: {
+      CSelect
+    },
 
-})
-export default class App extends Vue {
-  a = ['Label 1', 'Label 2', 'Label 3'];
-  b = [{ label: 'Label 1', value: 1}, { label: 'Label 2', value: 2 }];
-  c = [{ name: 'Label 1', id: 1}, { name: 'Label 2', id: 2 }];
-  d = { value1: 'Label 1', value2: 'Label 2' };
-  e = { value1: { title: 'Label 1', id: '1' }, value2: { title: 'Label 2', id: 2 } };
-  selected = 2;
+  })
+  export default class App extends Vue {
+    a = ['Label 1', 'Label 2', 'Label 3'];
+    b = [{label: 'Label 1', value: 1}, {label: 'Label 2', value: 2}];
+    c = [{name: 'Label 1', id: 1}, {name: 'Label 2', id: 2}];
+    d = {value1: 'Label 1', value2: 'Label 2'};
+    e = {value1: {title: 'Label 1', id: '1'}, value2: {title: 'Label 2', id: 2}};
+    selected = 2;
 
-  onInput(e) {
-    console.log(e);
+    onInput(e) {
+      console.log(e);
+    }
   }
-}
 </script>
 
 <style lang="scss">
